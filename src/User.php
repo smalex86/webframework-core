d7e91c315df68ad6e9f20b60b251846d10974aa9
Alexandre <mail_er@mail.ru>
2018-06-10 18:09:20 +0700

    Обновление User и UserMapper
    В класс User добавлены все поля из таблицы с пользователями. Добавлен конструктор.
    В класс UserMapper реализован метод для получения данных о пользователе из бд по идентификатору. Добавлен метод для получения данных о пользователе из сессии.

--- src/User.php
+++ src/User.php
@@ -1,18 +1,99 @@
 <?php
 
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * This file is part of the smalex86 package.
+ *
+ * (c) Alexandr Smirnov <mail_er@mail.ru>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace smalex86\webframework\core;
 
+use smalex86\webframework\core\ActiveRecord;
+
 /**
- * Description of User
+ * User
  *
- * @author Александр
+ * @author Alexandr Smirnov
  */
-class User {
-  //put your code here
+class User extends ActiveRecord {
+  
+  /** Идентификатор */
+  public $id;
+  /** Логин */
+  public $login;
+  /** Пароль */
+  public $password;
+  /** Идентификатор группы пользователя */
+  public $groupId;
+  /** Имя */
+  public $fname;
+  /** Отчество */
+  public $mname;
+  /** Фамилия */
+  public $lname;
+  /** Email */
+  public $email;
+  /** Код подтверждения email */
+  public $emailVerificationCode;
+  /** Подтвержден ли email */
+  public $emailVerified;
+  /** Дата регистрации */
+  public $registrationDate;
+  /** Ссылка на файл с аватаром */
+  public $avatar;
+  /** Номер телефона пользователя */
+  public $phone;
+  
+  /**
+   * Конструктор
+   * @param int $id
+   * @param string $login
+   * @param string $password
+   * @param int $groupId
+   * @param string $fname
+   * @param string $mname
+   * @param string $lname
+   * @param string $email
+   * @param string $emailVerificationCode
+   * @param int $emailVerified
+   * @param string $registrationDate
+   * @param string $avatar
+   * @param string $phone
+   */
+  public function __construct($id, $login, $password, $groupId, $fname, $mname, $lname, $email, 
+          $emailVerificationCode, $emailVerified, $registrationDate, $avatar, $phone) {
+    parent::__construct();
+    $this->id = $id;
+    $this->login = $login;
+    $this->password = $password;
+    $this->groupId = $groupId;
+    $this->fname = $fname;
+    $this->mname = $mname;
+    $this->lname = $lname;
+    $this->email = $email;
+    $this->emailVerificationCode = $emailVerificationCode;
+    $this->emailVerified = $emailVerified;
+    $this->registrationDate = $registrationDate;
+    $this->avatar = $avatar;
+    $this->phone = $phone;
+  }
+  
+  /**
+   * Получить идентификатор пользователя
+   * @return int
+   */
+  public function getId(): int
+  {
+    if (isset($this->id)) {
+      return $this->id;
+    } else {
+      return 0;
+    }
+  }
+  
+  
+  
 }
--- src/UserMapper.php
+++ src/UserMapper.php
@@ -11,15 +11,111 @@
 
 namespace smalex86\webframework\core;
 
+use smalex86\webframework\core\DataMapper;
+
 /**
- * Description of UserMapper
+ * UserMapper
  *
- * @author Александр
+ * @author Alexandr Smirnov <mail_er@mail.ru>
  */
-class UserMapper {
+class UserMapper extends DataMapper {
   
-  public function __construct() {
-    ;
+  public function __construct(Database $database, Session $session) {
+    parent::__construct($database, $session);
+    $this->tableName = 'core_user';
+  }
+
+  protected function beforeInsert() {
+    return;
+  }
+
+  protected function getFields() {
+    return;
   }
   
+  /**
+   * Возвращает данные о пользователе по его идентификатору из бд
+   * @param int $id
+   * @return \smalex86\webframework\core\User
+   */
+  public function getById(int $id) {
+    $query = sprintf('select * from %s where id = %u limit 1', $this->getTableName(), $id);
+    $row = $this->database->selectSingleRow($query, __FILE__.':'.__LINE__);
+    if ($row && is_array($row)) {
+      return new User($row['id'], $row['u_login'], $row['u_password'], $row['user_group_id'], 
+              $row['name_f'], $row['name_m'], $row['name_l'], $row['email'], 
+              $row['email_verification_code'], $row['email_verified'], $row['registration_date'], 
+              $row['avatar'], $row['phone']);
+    }
+    return null;
+  }
+  
+  /**
+   * Получает данные о пользователе сохраненном в сессии
+   * @return \smalex86\webframework\core\User
+   */
+  public function getActiveUser() {
+    $userSessionData = $this->session->getData('user');
+    if (isset($userSessionData['id'])) {
+      return $this->getById($userSessionData['id']);
+    }
+    return null;
+  }
+
+  public function getList() {
+    return;
+  }
+
+  public function processAction($postData = array()) {
+    return;
+  }
+  
+  /**
+   * Сохранение объекта в базе данных
+   * @param User $record
+   * @return 
+   */
+  public function save(ActiveRecord $record) {
+    if ($record->id) {
+      $query = sprintf('update %s set u_login="%s", u_password="%s", user_group_id=%u,'
+              . 'name_f="%s", name_m="%s", name_l="%s", email="%s", email_verification_code="%s",'
+              . 'email_verified=%u, registration_date="%s", avatar="%s", phone="%s" '
+              . 'where id=%u',
+              $this->tableName,
+              $record->login, 
+              $record->password,
+              $record->groupId,
+              $record->fname,
+              $record->mname,
+              $record->lname,
+              $record->email,
+              $record->emailVerificationCode,
+              $record->emailVerified,
+              $record->registrationDate,
+              $record->avatar,
+              $record->phone,
+              $record->id);
+      $this->database->updateSingle($query, __FILE__.':'.__LINE__);
+    } else {
+      $query = sprintf('insert into %s set u_login="%s", u_password="%s", user_group_id=%u,'
+              . 'name_f="%s", name_m="%s", name_l="%s", email="%s", email_verification_code="%s",'
+              . 'email_verified=%u, registration_date="%s", avatar="%s", phone="%s"',
+              $this->tableName,
+              $record->login, 
+              $record->password,
+              $record->groupId,
+              $record->fname,
+              $record->mname,
+              $record->lname,
+              $record->email,
+              $record->emailVerificationCode,
+              $record->emailVerified,
+              $record->registrationDate,
+              $record->avatar,
+              $record->phone);
+      $record->id = $this->database->insertSingle($query, __FILE__.':'.__LINE__);
+    }
+    return null;
+  }
+
 }
