1e969dfce5756bf908bb6f6e6442f16bce638d72
Alexandre <mail_er@mail.ru>
2018-07-14 01:42:45 +0700

    Issue #24 - Добавлена точка входа для ajax-запросов

---
+++ public/ajax.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * This file is part of the Smalex86 package.
+ *
+ * (c) Alexandr Smirnov <mail_er@mail.ru>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+include_once dirname(__DIR__) . '/bootstrap.php';
+
+$application->startAjaxManager(); // запуск обработки ajax-запроса
