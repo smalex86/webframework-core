fd1afa252bcd62397af3b39ce6ef219f9e640312
Alexandre <mail_er@mail.ru>
2018-10-12 22:00:35 +0700

    Обновление DatabasePDO и исключения

--- src/DatabasePDO.php
+++ src/DatabasePDO.php
@@ -119,7 +119,136 @@ class DatabasePDO {
             throw new DatabaseException($pe->getMessage(), 0, $pe);
         }
     }
+    
+    /**
+     * Выполняет запрос, который возвращает количество затронутых строк
+     * Следует использовать данный метод когда в запросе нет внешних данных
+     * @param string $query
+     * @return int
+     * @throws DatabaseException
+     */
+    public function exec($query) {
+      try {
+        return $this->pdo->exec($query);
+      } catch (\PDOException $pe) {
+        $this->logger->error('PDOException: ' . $pe->getMessage());
+        $this->lastErrorInfo = $this->pdo->errorInfo();
+        throw new DatabaseException($pe->getMessage(), 0, $pe);
+      }
+    }
 
+    /**
+     * Подготовка запроса и его выполнение
+     * @param string $query строка запроса sql с параметрами
+     * @param array $params ассоциативный массив с параметрами
+     * @return \PDOStatement
+     * @throws DatabaseExcepion
+     */
+    public function executeQuery(string $query, array $params) {
+      try {
+        $stmt = $this->pdo->prepare($query);
+        if ($stmt->execute($params)) {
+          return $stmt;
+        } else {
+          $this->lastErrorInfo = $this->pdo->errorInfo();
+          throw new DatabaseException('Выполнение execute() вызвало ошибку: ' 
+                  . var_export($this->lastErrorInfo, true));
+        }
+      } catch (\PDOException $e) {
+        $this->logger->error('PDOException: ' . $e->getMessage());
+        $this->lastErrorInfo = $this->pdo->errorInfo();
+        throw new DatabaseException($e->getMessage(), 0, $e);
+      }
+    }
+    
+    /**
+     * Возвращает строку из БД в виде ассоциативного массива
+     * @param string $query
+     * @param array $params
+     * @return array
+     * @throws DatabaseException
+     */
+    public function selectSingleRow(string $query, array $params) {
+      return $this->getExecutedStmt($query, $params)->fetch(\PDO::FETCH_LAZY);
+    }
+    
+    /**
+     * Возвращает набор строк из БД в виде массива ассоциативных массивов
+     * @param string $query
+     * @param array $params
+     * @return array
+     * @throws DatabaseException
+     */
+    public function selectMutlipleRows(string $query, array $params) {
+      return $this->getExecutedStmt($query, $params)->fetchAll(\PDO::FETCH_LAZY);
+    }
+    
+    /**
+     * Выполняет запрос на обновление данных и возвращает id вставленной записи
+     * @param string $query
+     * @param array $params
+     * @return int
+     * @throws DatabaseException
+     */
+    public function insertSingle(string $query, array $params) {
+      $this->getExecutedStmt($query, $params);
+      return $this->pdo->lastInsertId();
+    }
+    
+    /**
+     * Выполняет запрос на обновление записи и возвращает true в случае успеха
+     * @param string $query
+     * @param array $params
+     * @return boolean
+     * @throws DatabaseException
+     */
+    public function updateSingle(string $query, array $params) {
+      $this->getExecutedStmt($query, $params);
+      return true;
+    }
+    
+    /**
+     * Сейчас не делает никаких действий, реализован для совместимости
+     * Выполняет обработку строки для обезопасивания перед вставкой в sql запрос
+     * @param string $str
+     * @return string
+     */
+    public function getSafetyString(string $str) {
+      return $str;
+    }
+    
+    /**
+     * Сейчас не делает никаких действий, реализован для совместимости
+     * Выполняет обработку массива строк для обезопасивания перед вставкой в sql запрос
+     * @param array $data
+     * @return array
+     */
+    public function getSafetyStringList(array $data): array
+    {
+      foreach ($data as $key=>$value) {
+        if (is_string($value)) {
+          $data[$key] = $this->getSafetyString($value);
+        }
+      }
+      return $data;
+    }
+    
+    /**
+     * Получить выполненное выражение
+     * @param string $query
+     * @param array $params
+     * @return \PDOStatement
+     * @throws DatabaseException
+     */
+    private function getExecutedStmt(string $query, array $params) {
+      if (count($params)) {
+        $stmt = $this->executeQuery($query, $params);
+      } else {
+        $stmt = $this->query($query);
+      }
+      return $stmt;
+    }
+    
     /**
      * Сформировать dsn для подключения к БД
      * @param string $type pgsql || mysql
